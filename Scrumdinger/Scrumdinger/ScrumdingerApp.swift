004d88b57788f29139c479cbf28fba2286ad157a
MarvinLin <atimis19@gmail.com>
2022-06-15 00:10:47 +0800

    Scrumdinger-Section2
    Scrumdinger-Section2

---
+++ Scrumdinger/Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by cm0679 on 2022/6/15.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MeetingView()
+        }
+    }
+}
